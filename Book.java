0a64fea75d1000b5da852e451bf73a4fbea21287
karthikjogendra <149669314+karthikjogendra@users.noreply.github.com>
2025-07-20 21:25:00 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,43 @@
+
+public class Book {
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public Book(String title, String author, boolean isBorrowed) {
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = isBorrowed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void borrow() {
+        isBorrowed = true;
+    }
+
+    public void returnBook() {
+        isBorrowed = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + " | Author: " + author + " | Status: " + (isBorrowed ? "Borrowed" : "Available");
+    }
+}
